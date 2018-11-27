0fca41e09d3690e9ed3d09860ab5306d0549b8c2
Gokberk Hıdıroğlu <gokbey5801@gmail.com>
2018-11-27 23:43:19 +0300

    Stringler

---
+++ Stringler.kt
@@ -0,0 +1,17 @@
+
+
+
+fun main(args : Array<String>){
+
+
+    var baslik="Kotlin Ögreniyorum !"
+    println("$baslik")
+    println("Baslığın 5.karakteri " +baslik[4])
+    println(baslik.toUpperCase())
+    println("Split özelliği "+baslik.split(" "))
+    println("Trim özelliği "+baslik.trim())
+
+
+
+
+}
